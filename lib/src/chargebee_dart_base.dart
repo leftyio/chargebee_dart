26e6d336f4fdc950b66e27d5e211952ce98853aa
Lejard Hadrien <hadrien.lejard@gmail.com>
2018-01-31 14:13:50 +0100

    use completer and StreamController

--- lib/src/chargebee_dart_base.dart
+++ lib/src/chargebee_dart_base.dart
@@ -44,50 +44,57 @@ class ChargeBeeLoader {
   _ChargeBeeLoader _loader;
   ChargeBeeLoader._(this._loader);
 
-  void load(ChargeBeeLoadOptions options) {
-    final interop = (Function func) => func != null ? allowInterop(func) : null;
+  final _onAddIframe = new Completer<IFrameElement>();
+  final _onLoad = new Completer<IFrameDimensions>();
+  final _onResize = new StreamController<IFrameDimensions>();
+  final _onSuccess = new Completer<IFrameElement>();
+  final _onCancel = new Completer<IFrameElement>();
+
+  void load() {
+    final interop = (Function func) => allowInterop(func);
 
     _loader.load(new _ChargeBeeLoadOptions(
-        addIframe: interop(options.addIframe),
-        onLoad: interop(options.onLoad),
-        onResize: interop(options.onResize),
-        onSuccess: interop(options.onSuccess),
-        onCancel: interop(options.onCancel)));
+        addIframe: interop((iframe) => _onAddIframe.complete(iframe)),
+        onLoad: interop((iframe, width, height) =>
+            _onLoad.complete(new IFrameDimensions(iframe, width, height))),
+        onResize: interop((iframe, width, height) =>
+            _onResize.add(new IFrameDimensions(iframe, width, height))),
+        onSuccess: interop((iframe) => _onSuccess.complete(iframe)),
+        onCancel: interop((iframe) => _onCancel.complete(iframe))));
   }
-}
 
-class ChargeBeeLoadOptions {
+  void dispose() {
+    _onResize.close();
+  }
+
   /// This function will be called when iframe is created.
   /// addIframe callback will recieve iframe as parameter.
   /// you can use this iframe to add iframe to your page.
   /// Loading image in container can also be showed in this callback.
   /// Note: visiblity will be none for the iframe at this moment
-  final IframeCallback addIframe;
+  Future<IFrameElement> get onAddIframe => _onAddIframe.future;
 
   /// This function will be called once when iframe is loaded.
   /// Since checkout pages are responsive you need to handle only height.
-  final IframeDimensionCallback onLoad;
+  Future<IFrameDimensions> get onLoad => _onLoad.future;
 
   /// This will be triggered when any content of iframe is resized.
-  final IframeDimensionCallback onResize;
+  /// if onResize is used [ChargeBeeLoader] must be dispose calling [ChargeBeeLoader.dispose]
+  Stream<IFrameDimensions> get onResize => _onResize.stream;
 
   /// This will be triggered when checkout is complete.
-  final IframeCallback onSuccess;
+  Future<IFrameElement> get onSuccess => _onSuccess.future;
 
   /// This will be triggered when user clicks on cancel button.
-  final IframeCallback onCancel;
-
-  ChargeBeeLoadOptions(
-      {this.addIframe,
-      this.onLoad,
-      this.onResize,
-      this.onSuccess,
-      this.onCancel});
+  Future<IFrameElement> get onCancel => _onCancel.future;
 }
 
-typedef void IframeCallback(IFrameElement iframe);
-typedef void IframeDimensionCallback(
-    IFrameElement iframe, num width, num height);
+class IFrameDimensions {
+  final IFrameElement iframe;
+  final num height;
+  final num width;
+  IFrameDimensions(this.iframe, this.width, this.height);
+}
 
 final _api = "https://js.chargebee.com/v1/chargebee.js";
 
